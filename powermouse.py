cbcd500379a24c2656ed6fd3c7666fd4f1ea7619
TheBdouilleur <bdouilleur@gmail.com>
2020-06-07 20:14:57 +0200

    Added new nav functions and respective notifications, cleaned code

--- powermouse.py
+++ powermouse.py
@@ -5,6 +5,7 @@ from pynput.keyboard import Listener, Key, KeyCode
 import pyautogui
 from easy_notify import notify
 
+screenWidth, screenHeight = pyautogui.size()
 pyautogui.FAILSAFE = False
 
 SMALL_MOVEMENT = 40
@@ -15,30 +16,48 @@ DOWN_KEYS = [KeyCode.from_char('j'), Key.down]
 UP_KEYS = [KeyCode.from_char('k'), Key.up]
 RIGHT_KEYS = [KeyCode.from_char('l'), Key.right]
 
-QUIT_KEYS = [KeyCode.from_char('x'), Key.esc]
+LEFTMOST_KEYS = [KeyCode.from_char('0'), Key.home]
+BOTTOM_KEYS = [KeyCode.from_char('G'), Key.page_down]
+TOP_KEYS = [KeyCode.from_char('gg'), Key.page_down]
+RIGHTMOST_KEYS = [KeyCode.from_char('$'), Key.end]
+
+QUIT_KEYS = [KeyCode.from_char('x'), KeyCode.from_char('q'), Key.esc]
 
 CLICK_KEYS = [KeyCode.from_char('c'), Key.enter]
 
 
 def on_press(key):
     """Get the key pressed and run some code depending on it"""
+    currentX, currentY = pyautogui.position()
 
     if key in LEFT_LEYS:
         pyautogui.move(-SMALL_MOVEMENT, 0)
-
     if key in DOWN_KEYS:
         pyautogui.move(0, SMALL_MOVEMENT)
-
     if key in UP_KEYS:
         pyautogui.move(0, -SMALL_MOVEMENT)
-
     if key in RIGHT_KEYS:
         pyautogui.move(SMALL_MOVEMENT, 0)
 
-    if key == Key.enter:
+    if key in LEFTMOST_KEYS:
+        pyautogui.moveTo(0, currentY)
+        notify("Powermouse", "Moved to left of the screen")
+    if key in BOTTOM_KEYS:
+        pyautogui.moveTo(currentX, screenHeight)
+        notify("Powermouse", "Moved to bottom of screen")
+    if key in TOP_KEYS:
+        pyautogui.moveTo(screenWidth, currentY)
+        notify("Powermouse", "Moved to top of screen")
+    if key in RIGHTMOST_KEYS:
+        pyautogui.moveTo(0, currentY)
+        notify("Powermouse", "Moved to right of screen")
+
+    if key in CLICK_KEYS:
         pyautogui.click()
+        notify("Powermouse", f"Clicked at position {pyautogui.position()}")
 
     if key in QUIT_KEYS:
+        notify("Powermouse", "Quitting")
         exit()
 
 
@@ -47,4 +66,3 @@ if __name__ == "__main__":
 
     with Listener(on_press=on_press) as listener:
         listener.join()
-
